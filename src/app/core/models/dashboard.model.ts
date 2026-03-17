b9e7dd18f911ad5ce41a49fa85660c0620351fdc
Hichamelhmaydi <ct1594065@gmail.com>
2026-03-17 10:46:45 +0000

    add AdminDashboard interface to model dashboard data

---
+++ src/app/core/models/dashboard.model.ts
@@ -0,0 +1,12 @@
+export interface AdminDashboard {
+  totalReservations: number;
+  reservationsEnAttente: number;
+  reservationsConfirmees: number;
+  reservationsCompletees: number;
+  reservationsAnnulees: number;
+  totalVoyages: number;
+  voyagesDisponibles: number;
+  totalVoyageurs: number;
+  voyageursActifs: number;
+  voyageursBloques: number;
+}
