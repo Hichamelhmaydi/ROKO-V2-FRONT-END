1340fcd68c5a6aa5a2accb80eb990d7e55063f6b
Hichamelhmaydi <ct1594065@gmail.com>
2026-03-17 10:48:18 +0000

    add User, LoginRequest, RegisterRequest, and AuthResponse interfaces for user authentication and management

---
+++ src/app/core/models/user.model.ts
@@ -0,0 +1,35 @@
+export interface User {
+  id: number;
+  email: string;
+  nom: string;
+  prenom: string;
+  telephone?: string;
+  status?: string;
+  idNational?: string;
+  dateExpiration?: string;
+  role: 'ADMIN' | 'VOYAGEUR' | 'USER' | 'ROLE_ADMIN' | 'ROLE_VOYAGEUR' | 'ROLE_USER' | string;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface RegisterRequest {
+  email: string;
+  password: string;
+  nom: string;
+  prenom: string;
+  telephone: string;
+}
+
+export interface AuthResponse {
+  token: string;
+  type: string;
+  id: number;
+  email: string;
+  nom: string;
+  prenom: string;
+  role: string;
+  message?: string;
+}
