c03b746fd4f48d331f39ca6194b60dc5b129367a
Hichamelhmaydi <ct1594065@gmail.com>
2026-03-17 10:47:52 +0000

    add Reservation and ReservationRequest interfaces to model reservation data

---
+++ src/app/core/models/reservation.model.ts
@@ -0,0 +1,44 @@
+import { Activite } from './voyage.model';
+
+export type ReservationStatus =
+  | 'EN_ATTENTE'
+  | 'CONFIRMEE'
+  | 'PAYEE'
+  | 'COMPLETEE'
+  | 'ANNULEE'
+  | string;
+
+export interface Reservation {
+  id: number;
+  voyageId: number;
+  userId?: number;
+  voyageNom?: string;
+  voyageDestination?: string;
+  voyageDateDepart?: string;
+  voyageDateRetour?: string;
+  userNom?: string;
+  userPrenom?: string;
+  userEmail?: string;
+  nombrePersonnes: number;
+  statut?: ReservationStatus;
+  dateReservation?: string;
+  dateConfirmation?: string;
+  dateAnnulation?: string;
+  dateCompletion?: string;
+  prixBase?: number;
+  prixActivites?: number;
+  montantTotal?: number;
+  commentaire?: string;
+  activitesOptionnellesIds?: number[];
+  activites?: Activite[];
+  motifAnnulation?: string;
+  paiementEffectue?: boolean;
+  datePaiement?: string;
+}
+
+export interface ReservationRequest {
+  voyageId: number;
+  nombrePersonnes: number;
+  commentaire?: string;
+  activitesOptionnellesIds?: number[];
+}
